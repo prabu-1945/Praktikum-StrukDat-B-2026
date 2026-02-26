d7d28b915723a04eb823bb7d899c78eab15ae9ac
prabu-1945 <prabusang531@gmail.com>
2026-02-26 23:13:32 +0700

    upload latihan 4

---
+++ pertemuan4/latihan/test.py
@@ -0,0 +1,7 @@
+# test.py - jalankan setelah install colorama
+from colorama import Fore, Back, Style, init
+init()
+print(Fore.RED + 'Teks Merah')
+print(Fore.GREEN + 'Teks Hijau')
+print(Fore.BLUE + Back.YELLOW + 'Teks Biru, Bg Kuning')
+print(Style.RESET_ALL + 'Kembali Normal')
